9daeb16dd6cf6497e0bcc05d16ccad2bf0c1a25c
Jaime D. Alvarez Gerding <jalvarez@ieee.org>
2020-06-01 00:52:46 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## This function creates four functions that allow for the 
+## calculation and recovery of the inverse of a matrix
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.na(i[1,1])) {              ## checks if the stored value
+                                    ## of the inverse has a NA in
+                                    ## the first element. That would
+                                    ## mean there is no inverse
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()                   ## obtains the matrix
+  i <- solve(data, ...)             ## calculates the inverse
+  x$setinv(i)                       ## caches the inverse
+  i
 }
