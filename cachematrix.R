7aec5165c78ae524fe4e77c49e7a1771a7dfb2e2
Jaime D. Alvarez Gerding <jalvarez@ieee.org>
2020-06-01 02:56:41 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,27 @@
 ## This function creates four functions that allow for the 
 ## calculation and recovery of the inverse of a matrix
 
+function(x = matrix()) {
+  
+  i <- matrix(NA,nrow(x), ncol(x))  ## initalize a matrix with NA
+                                    ## values for storing the 
+                                    ## inverse
+  set <- function(y) {              ## fnction for changing the 
+                                    ## value of the matrix
+    x <<- y
+    i <<- matrix(NA,nrow(x), ncol(x)) ## resets the inverse
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv ## called from cacheSolve to 
+                                    ## set the inverse
+  getinv <- function() i            ## called from cacheSolve to
+                                    ## the stored value of the
+                                    ## inverse
+  list(set = set, get = get,        ## Set names for allowing the 
+                                    ## functions to be called
+       setinv = setinv,
+       getinv = getinv)
+}
 
 
 cacheSolve <- function(x, ...) {
